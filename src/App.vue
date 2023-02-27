d14dc8e40754c57450be90840c07ca8cab8d5213
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-27 14:59:41 +0000

    define App component with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { reactive, ref } from "vue";
+
+// using ref()
+// const name = ref<string>("Link");
+// const age = ref<string | number>(20);
+
+// using reactive()
+const state = reactive({
+  name: 'Link',
+  age: 20 as string | number
+})
+
+function changeName(newName: string) {
+  state.name = newName
+}
+
+function changeAge(newAge: string | number) {
+  state.age = newAge
+}
 </script>
 
 <template>
-  <h1>Hello Friends</h1>
+  <h1>Hello {{ state.name }} - {{ state.age }}</h1>
+  <button @click="changeName('Zelda')">Change Name</button>
+  <button @click="changeAge(30)">Change Age</button>
 </template>
 
 <style scoped>
