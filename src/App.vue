e5df8e75056cd9fb5c44ca8904774cf6d3f791f8
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-27 15:09:00 +0000

    add type interface

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
 import { reactive, ref } from "vue";
+import type Job from "./types/Job";
 
 // using ref()
 // const name = ref<string>("Link");
 // const age = ref<string | number>(20);
 
 // using reactive()
-const state = reactive({
-  name: 'Link',
-  age: 20 as string | number
-})
+// const state = reactive({
+//   name: 'Link',
+//   age: 20 as string | number
+// })
 
-function changeName(newName: string) {
-  state.name = newName
-}
-
-function changeAge(newAge: string | number) {
-  state.age = newAge
-}
+// using ref with custom type
+const jobs = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' },
+])
 </script>
 
 <template>
-  <h1>Hello {{ state.name }} - {{ state.age }}</h1>
-  <button @click="changeName('Zelda')">Change Name</button>
-  <button @click="changeAge(30)">Change Age</button>
+  <h1>{{ jobs[0].title }} from {{ jobs[0].location }}</h1>
 </template>
 
 <style scoped>
