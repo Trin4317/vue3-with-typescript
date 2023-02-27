c3648ce29536e44346bd07c7cc07c06ca686defe
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-27 16:02:06 +0000

    format code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { reactive, ref } from 'vue'
 import JobList from './components/JobList.vue'
-import type Job from "./types/Job";
+import type Job from './types/Job'
 
 // using ref()
 // const name = ref<string>("Link");
@@ -19,7 +19,7 @@ const jobs = ref<Job[]>([
   { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
   { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
   { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' },
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
 ])
 </script>
 
@@ -27,5 +27,4 @@ const jobs = ref<Job[]>([
   <JobList :jobs="jobs" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
