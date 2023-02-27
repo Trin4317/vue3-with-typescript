0f1664440b7e49ffc5d3149ec95530e1b7997a1d
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-27 16:21:09 +0000

    add css

--- src/App.vue
+++ src/App.vue
@@ -37,4 +37,21 @@ const handleClick = (term: OrderTerm) => {
   <JobList :jobs="jobs" :order="order" />
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
